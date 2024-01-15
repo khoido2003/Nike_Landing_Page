d6601096122c0591adbe1378ed10c735f4e43521
Khoi Do <khoizpro31@gmail.com>
2024-01-15 23:23:47 +0700

    Update Service Section

--- app/sections/Services.tsx
+++ app/sections/Services.tsx
@@ -1,3 +1,12 @@
+import { services } from "../constants";
+import ServiceCard from "../ui/ServiceCard";
+
 export default function Services() {
-  return <div>Services</div>;
+  return (
+    <section className="max-container flex flex-1 flex-wrap justify-center gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
 }
--- app/ui/ServiceCard.tsx
+++ app/ui/ServiceCard.tsx
@@ -1,5 +1,20 @@
-function ServiceCard() {
-  return <div>ServiceCard</div>;
+import Image from "next/image";
+import { Service } from "../constants";
+
+function ServiceCard({ imgURL, subtext, label }: Service) {
+  return (
+    <div className="w-full min-w-[350px] flex-1 rounded-[20px] px-10 py-16 shadow-3xl sm:w-[350px] ">
+      <div className="flex h-11 w-11 items-center justify-center rounded-full bg-coral-red">
+        <Image src={imgURL} alt={label} width={24} height={24} />
+      </div>
+      <h3 className="mt-5 font-palanquin text-3xl font-bold leading-normal">
+        {label}
+      </h3>
+      <p className="mt-3 break-words font-montserrat text-lg leading-normal text-slate-gray">
+        {subtext}
+      </p>
+    </div>
+  );
 }
 
 export default ServiceCard;
